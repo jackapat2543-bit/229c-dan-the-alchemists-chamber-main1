4fc26077ba02b7b2966d0b953f1b13e72b5813b6
jackapat2543-bit <jackapat2543@gmail.com>
2026-03-28 17:45:57 +0700

    Test succeed

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+    public void start_game ()
+    {
+      SceneManager.LoadScene(0);
+    }
+    public void test_game ()
+    {
+      SceneManager.LoadScene(1);
+    }
+}

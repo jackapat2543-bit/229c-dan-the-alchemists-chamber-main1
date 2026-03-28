5a6b3ad11a27ffdb51ea76a53668d3d9f145c10c
TrinityJN <pathitta.khan@bumail.net>
2026-03-28 23:20:39 +0700

    fix setting

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class menu : MonoBehaviour
 {
-    public void start_game ()
+    public void start_game()
     {
-      SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
     }
-    public void test_game ()
+    public void test_game()
     {
-      SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);
+    }
+    public void Credit_game()
+    {
+        SceneManager.LoadScene(2);
     }
 }
